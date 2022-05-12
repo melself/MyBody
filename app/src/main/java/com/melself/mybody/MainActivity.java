5eb78a11c55f78f1d7233650ebfcfafc5fc65360
Eugene <maskot2002@yandex.ru>
2022-05-13 01:50:40 +0300

    Update 1.2 Add develop HorizontalScroll in Home Fragment

--- app/src/main/java/com/melself/mybody/MainActivity.java
+++ app/src/main/java/com/melself/mybody/MainActivity.java
@@ -1,10 +1,12 @@
 package com.melself.mybody;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -18,20 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        replaceFragment(new HomeFragment());
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.darkGray));
+        }
+
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.home);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.home:
                     replaceFragment(new HomeFragment());
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.darkGray));
+                    }
                     break;
 
                 case R.id.trainer:
                     replaceFragment(new TrainerFragment());
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.dark));
+                    }
                     break;
 
                 case R.id.profile:
                     replaceFragment(new ProfileFragment());
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.dark));
+                    }
                     break;
             }
             return true;
@@ -41,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     public void replaceFragment(Fragment fragment){
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+        //ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
         ft.replace(R.id.mainFrameForFragments, fragment);
         ft.commit();
     }
